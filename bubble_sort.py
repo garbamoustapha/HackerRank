6716db666290fa6e5385f710a5274f32e39e71ec
garbamoustapha <garbamoustapha0@gmail.com>
2026-02-26 22:22:18 +0100

    Add bubble_sort.py to implement a function for sorting an array using bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def countSwaps(a):
+    swap = 0
+    n = len(a) 
+    
+    for i in range(n):
+        for j in range(0, n - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swap += 1
+                
+    print(f"Array is sorted in {swap} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[n-1]}")
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
